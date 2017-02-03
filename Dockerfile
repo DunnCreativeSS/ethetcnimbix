8e05a164d1184fff4a90a5fb0b3badf78ac840a4
Ubuntu <ubuntu@ip-172-31-31-87.us-west-2.compute.internal>
2017-02-03 13:20:35 +0000

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM nimbix/base-ubuntu-nvidia 
+RUN wget https://www.dropbox.com/s/vy4cg3e9w5g4g49/claymore.zip
+RUN unzip claymore.zip
+ADD script.sh /
+RUN script.sh
---
+++ script.sh
@@ -0,0 +1,2 @@
+claymore/ethdcrminer64 -epool us1.ethermine.org:4444 -ewal 0x77ea528a09b556726998e826200c1aa6a0724c26.army -epsw x -dpool dcr.suprnova.cc:2252 -dwal jrsdunn.suprnovajare -dpsw w0rdp4ss -esm 1 -mode 0 -tt 70
+
